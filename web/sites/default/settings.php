f4558251a60f478d191a9a53322d36275b49e7ce
rwofqual <robert.wild@ofqual.gov.uk>
2025-12-02 12:04:18 +0000

    61132 - Cleanup repo of composer controlled file and theme assets (#16)
    * Refactor: Mostly Drupal composer related...
    
    - Sets correct project name
    - Stops version locking in composer.json and moves this to .lock
    - Updates most Drupal contrib and core module
    - Sets the path for the config import (will break Bitnami for now)
    - Updates config firectory for new syntax
    - Starts the git cleanup
    
    * Tidying up README.md files for syntax and less shouty headings
    
    * Cleaning repo
    
    * Update .gitignore
    
    * Updating composer to utilise drupal scafffold
    
    * Fixing fatal error on install

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -256,7 +256,7 @@ $databases = [];
  * directory in the public files path. The setting below allows you to set
  * its location.
  */
-# $settings['config_sync_directory'] = '/directory/outside/webroot';
+ $settings['config_sync_directory'] = '../config/sync';
 
 /**
  * Settings:
@@ -848,12 +848,14 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 
-$settings['config_sync_directory'] = '/bitnami/drupal/sites/default/files/config/sync';
-
 $config['openid_connect.client.entraid']['settings']['client_id'] = getenv('OPENID_CLIENT_ID');
 $config['openid_connect.client.entraid']['settings']['client_secret'] = getenv('OPENID_CLIENT_SECRET');
+$config['openid_connect.client.entraid']['settings']['authorization_endpoint'] = getenv('OPENID_AUTH_ENDPOINT');
+$config['openid_connect.client.entraid']['settings']['token_endpoint'] = getenv('OPENID_TOKEN_ENDPOINT');
 $config['openid_connect.client.entraid']['settings']['userinfo_endpoint'] = getenv('OPENID_USERINFO_ENDPOINT');
 
-$tenantId = getenv('AZURE_TENANT_ID');
-$config['openid_connect.client.entraid']['settings']['authorization_endpoint'] = 'https://login.microsoftonline.com/' . $tenantId . '/oauth2/v2.0/authorize';
-$config['openid_connect.client.entraid']['settings']['token_endpoint'] = 'https://login.microsoftonline.com/' . $tenantId . '/oauth2/v2.0/token';
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
