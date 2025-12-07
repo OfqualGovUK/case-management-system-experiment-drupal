3d4ad77c611be1d3c62679d6bc9f4527c4fa8aba
harshapreddy <harshareddy.leeds@gmail.com>
2025-12-07 21:15:31 +0000

    Add search test

--- tests/Pages/homePage.ts
+++ tests/Pages/homePage.ts
@@ -1,8 +1,26 @@
-class homePage{
-    page: any;
+import { expect, Locator, Page } from '@playwright/test';
 
-    constructor(page: any){
+class homePage{
+    page: Page;
+    searchInput: Locator;
+    tableBody: Locator;
+    rowsWithoutFilteredAttr: Locator;
+
+    constructor(page: Page){
         this.page = page;
+        this.searchInput = page.locator('.cds--search-input');
+        this.tableBody = page.locator('cds-table-body'); 
+        this.rowsWithoutFilteredAttr = this.tableBody.locator('cds-table-row:not([filtered])');
+    }
+
+    async search(text: string){
+
+        await this.searchInput.fill(text);
+        await expect(this.searchInput).toHaveValue(text);
+    }
+
+    async searchResultsCount(expected: number){
+        await expect(this.rowsWithoutFilteredAttr).toHaveCount(expected);
     }
 }
 
--- tests/sample-test.spec.ts
+++ tests/sample-test.spec.ts
@@ -1,13 +1,28 @@
 import { test, expect } from '@playwright/test';
 import HomePage from './Pages/homePage';
 
-let homePage;
+let homePage: HomePage;
 const url = 'localhost';
+
+//Load homepage before each test
+test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page);
+    await homePage.page.goto(url);
+});
 
 //case management homepage
 test('Case Management Home Page Test', async ({ page }) => {
-    homePage = new HomePage(page);
-    await homePage.page.goto(url);
     await expect(page).toHaveTitle("Welcome! | Ofqual");
+});
 
+//case management search by Title
+test('Case Management Search by Title Test', async ({ page }) => {
+    await homePage.search('Statement');
+    await homePage.searchResultsCount(1);
+});
+
+//case management search by status
+test('Case Management Search by Status Test', async ({ page }) => {
+    await homePage.search('Triage');
+    await homePage.searchResultsCount(2);
 });
