40f2ab4320bc1ff01ec25f44ea51ca2902574dda
Robert Wild <robert.wild@ofqual.gov.uk>
2026-01-14 14:08:26 +0000

    61137: Adding form modes and some scope for additional content inside forms and diplay

---
+++ web/modules/custom/external_entities_suitecrm/src/Plugin/Field/FieldWidget/FormModeLinksWidget.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\external_entities_suitecrm\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'form_mode_links_widget' widget.
+ *
+ * @FieldWidget(
+ *   id = "form_mode_links_widget",
+ *   label = @Translation("Form Mode Links Configuration"),
+ *   field_types = {
+ *     "form_mode_links"
+ *   }
+ * )
+ */
+class FormModeLinksWidget extends WidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    // This field doesn't need a widget on the entity edit form
+    // The configuration is done at the field settings level
+    $element['value'] = [
+      '#type' => 'value',
+      '#value' => '',
+    ];
+
+    return $element;
+  }
+
+}
